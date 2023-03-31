84295001ac14a4093f3897c332eddaa56135bacb
Alexander Vargas <cavale610@gmail.com>
2023-03-31 18:31:24 -0500

    first commit

---
+++ ExampleGuide/Data/Migrations/20230331212533_table-employee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExampleGuide.Data.Migrations
+{
+    public partial class tableemployee : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employee",
+                columns: table => new
+                {
+                    EmployeeId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Phone = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employee", x => x.EmployeeId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employee");
+        }
+    }
+}
---
+++ ExampleGuide/Models/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations; // Esto nos permite hacer la validacion de Required
+
+namespace ExampleGuide.Models
+{
+    public class Employee
+    {
+
+        public Guid EmployeeId { get; set; }
+
+
+        [Required(ErrorMessage ="Campo {0} requerido")] // Validacion de requerido
+        [Display(Name ="Nombre")] // Como se reconocera el campo en el front
+        public string Name { get; set; }
+
+
+
+        [Required(ErrorMessage = "Campo {0} requerido")] // Validacion de requerido
+        [Display(Name = "Apellido")] // Como se reconocera el campo en el front
+        public string LastName { get; set; }
+
+
+        [Required(ErrorMessage = "Campo {0} requerido")] // Validacion de requerido
+        [Display(Name = "Telefono")] // Como se reconocera el campo en el front
+        public string Phone { get; set; }
+
+
+        [Required(ErrorMessage = "Campo {0} requerido")] // Validacion de requerido
+        [Display(Name = "Estado")] // Como se reconocera el campo en el front
+        public int Status { get; set; }
+        
+        
+    }
+}
